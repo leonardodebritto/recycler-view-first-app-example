87b799d2beda824410882dd4fb9cab95ce84406b
leonardodebritto <britto.leonardo@outlook.com>
2021-06-02 11:29:07 -0300

    applying the kotlin plugin to module level build.gradle to solve the reference problem

--- app/src/main/java/br/com/recyclerviewfirstapp/ExampleAdapter.kt
+++ app/src/main/java/br/com/recyclerviewfirstapp/ExampleAdapter.kt
@@ -1,4 +1,12 @@
 package br.com.recyclerviewfirstapp
 
-class ExampleAdapter {
+import android.view.View
+import android.widget.ImageView
+import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.synthetic.main.example_item.view.*
+
+class ExampleAdapter : RecyclerView.Adapter()  {
+    class ExampleViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val imageView: ImageView = itemView.image_view
+    }
 }
